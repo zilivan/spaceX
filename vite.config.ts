91a6c81a4ad72292179ed1c1bab2de0e5586c50c
zilivan <stroielectromir@mail.ru>
2025-10-22 15:43:45 +0300

    gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+//'/spaseX/'  gh-page
+//'/'   vercel
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vegetable/',
+
+    base: '/spaseX/',
 });
